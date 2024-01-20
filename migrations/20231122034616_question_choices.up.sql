7f288f406e17424ea398ed0a708d10aadf1a9dfa
Kevin Vargas <vargaskevv@gmail.com>
2024-01-19 22:41:45 -0400

    Delete go files, we're moving to Elysia

--- migrations/20231122034616_question_choices.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS question_choices(
-  id UUID PRIMARY KEY,
-  question_id UUID REFERENCES questions(id) ON DELETE CASCADE NOT NULL,
-  text TEXT NOT NULL,
-  is_correct BOOLEAN NOT NULL,
-  created_at TIMESTAMP DEFAULT current_timestamp
-);
