3cd37df0a21d0d1708ccd924ea66ee8752f1e93b
Kevin Vargas <vargaskevv@gmail.com>
2024-01-24 17:58:29 -0400

    Questions request with choices and votes

--- src/apps/trivue/handlers/getQuestions.ts
+++ src/apps/trivue/handlers/getQuestions.ts
@@ -1,6 +1,8 @@
 import { Type } from "@sinclair/typebox";
-import { eq, sql, inArray } from "drizzle-orm";
+import { eq, inArray, sql } from "drizzle-orm";
+import { AppError } from "../../../appError";
 import { RootServer } from "../../../types";
+import { HttpStatusCode } from "../../../utils";
 import {
 	Author,
 	Choice,
@@ -12,11 +14,17 @@ import {
 } from "../schema";
 import { Level, VoteType } from "../types";
 
+// TODO: change these values. maxQuestions = 10, voteDifference = 10;
 const voteDifference = 1;
+const maxQuestions = 2;
 
 type QuestionItem = Omit<Question, "authorId" | "level"> & {
 	author: Author | null;
 	choices: Omit<Choice, "questionId">[];
+	votes: {
+		positive: number;
+		negative: number;
+	};
 };
 
 const queryString = Type.Object({
@@ -33,11 +41,18 @@ export async function getQuestions(app: RootServer) {
 		},
 		async function(req, res) {
 			const query = req.query;
+
 			const votesQuery = this.db
 				.select({
 					questionId: votes.questionId,
-					positive: sql<number>`COALESCE(SUM(CASE WHEN ${votes.type} = ${VoteType.positive} THEN 1 END), 0)::int`.as('positive'),
-					negative: sql<number>`COALESCE(SUM(CASE WHEN ${votes.type} = ${VoteType.negative} THEN 1 END), 0)::int`.as('negative'),
+					positive:
+						sql<number>`COALESCE(SUM(CASE WHEN ${votes.type} = ${VoteType.positive} THEN 1 END), 0)::int`.as(
+							"positive",
+						),
+					negative:
+						sql<number>`COALESCE(SUM(CASE WHEN ${votes.type} = ${VoteType.negative} THEN 1 END), 0)::int`.as(
+							"negative",
+						),
 				})
 				.from(votes)
 				.having(
@@ -46,53 +61,51 @@ export async function getQuestions(app: RootServer) {
 				)
 				.groupBy(votes.questionId)
 				.as("votesQuery");
-			const dbQuestions = this.db
+
+			const dbQuestions = await this.db
 				.with(votesQuery)
 				.select()
 				.from(questions)
-				.innerJoin(votesQuery, eq(questions.id, votesQuery.questionId));
-			console.log(dbQuestions.toSQL());
-			const dbVotes = await dbQuestions;
-			res.send(dbVotes);
-			return;
-			const dbQuery = this.db
-				.select({
-					question: questions,
-					author: authors,
-					choice: choices,
-				})
-				.from(questions)
 				.where(eq(questions.level, query.level))
+				.innerJoin(votesQuery, eq(questions.id, votesQuery.questionId))
 				.leftJoin(authors, eq(questions.authorId, authors.id))
-				.innerJoin(choices, eq(choices.questionId, questions.id));
+				.orderBy(sql`RANDOM()`)
+				.limit(maxQuestions);
 
-			console.log("QUERY:", dbQuery.toSQL());
-			const results = await dbQuery;
+			if (dbQuestions.length < maxQuestions) {
+				throw new AppError(
+					HttpStatusCode.UNPROCESSABLE_ENTITY,
+					`Sorry, at least 10 ${query.level
+					} questions with ${voteDifference} positive points are required. Currently, only ${dbQuestions.length
+					} ${dbQuestions.length === 1 ? "question meets" : "questions meet"
+					} this condition. You can help by posting questions or rating them.`,
+				);
+			}
 
-			const dataMap = results.reduce<Record<string, QuestionItem>>(
-				(acc, res) => {
-					if (!acc[res.question.id]) {
-						acc[res.question.id] = {
-							id: res.question.id,
-							text: res.question.text,
-							seconds: res.question.seconds,
-							explanation: res.question.explanation,
-							createdAt: res.question.createdAt,
-							author: res.author,
-							choices: [],
-						};
-					}
-					acc[res.question.id].choices.push({
-						id: res.choice.id,
-						text: res.choice.text,
-						isCorrect: res.choice.isCorrect,
-					});
-					return acc;
-				},
-				{},
-			);
+			const dbQuestionIds = dbQuestions.map((q) => q.questions.id);
+			const dbChoices = await this.db
+				.select()
+				.from(choices)
+				.where(inArray(choices.questionId, dbQuestionIds));
 
-			res.send(Object.values(dataMap));
+			const result: Array<QuestionItem> = [];
+			for (const q of dbQuestions) {
+				const item: QuestionItem = {
+					id: q.questions.id,
+					text: q.questions.text,
+					explanation: q.questions.explanation,
+					seconds: q.questions.seconds,
+					createdAt: q.questions.createdAt,
+					author: q.authors,
+					votes: q.votesQuery,
+					choices: [],
+				};
+				item.choices = dbChoices
+					.filter((c) => c.questionId === item.id)
+					.map((c) => ({ id: c.id, text: c.text, isCorrect: c.isCorrect }));
+				result.push(item);
+			}
+			res.send(result);
 		},
 	);
 }
